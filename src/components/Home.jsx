6fba5221ad393dbe5d5d40ccb23eba85b4939cea
Prayag Gupta <guptaprayag16@gmail.com>
2024-03-15 00:31:52 +0530

    project- 2 chakra UI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import img1 from '../assets/1.jpg';
@@ -12,7 +12,7 @@ const headingOptions = {
   pos: 'absolute',
   left: '50%',
   top: '50%',
-  transform: 'translate(-50%, -50%)',
+  transform: 'translate(-50%,-50%)',
   textTransform: 'uppercase',
   p: '4',
   size: '4xl',
@@ -22,13 +22,14 @@ const Home = () => {
   return (
     <Box>
       <MyCarousel />
-      <Container maxW={'container.x1'} minH={'100vh'} p="16">
+
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
         <Heading
           textTransform={'uppercase'}
           py="2"
           w={'fit-content'}
           borderBottom={'2px solid'}
-          m={'auto'}
+          m="auto"
         >
           Services
         </Heading>
@@ -40,20 +41,27 @@ const Home = () => {
           direction={['column', 'row']}
         >
           <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
-          <Text letterSpacing={'widest'} lineHeight={'190%'} padding={["4", "16"]} textAlign={"center"}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Dignissimos fuga porro, modi sit labore reprehenderit explicabo unde
-            est illo eum quos obcaecati accusamus cupiditate excepturi maxime
-            soluta ipsum, quaerat odio, doloremque necessitatibus quia! Autem
-            voluptates sunt, quibusdam tempora doloremque corporis dolores
-            molestiae sequi perferendis cumque voluptatum earum expedita aliquam
-            facilis pariatur, labore placeat dolorem eos obcaecati saepe. Labore
-            consectetur quia delectus expedita perspiciatis, nesciunt hic
-            dolores ipsam illum inventore, id officiis sequi modi sapiente, quis
-            explicabo? Beatae, quam quaerat optio aspernatur similique amet
-            aliquid veniam ullam debitis sint temporibus tenetur quod ea illo
-            quae sed! Quia, accusamus. Asperiores, tempore consectetur?
-            Veritatis?
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nemo
+            tempora deleniti quod, est eos veniam autem asperiores rem, nam
+            laudantium ut, similique ducimus amet sequi et voluptas iusto rerum
+            at aliquid numquam corporis ipsa reprehenderit? Porro iusto magnam,
+            sit vero deleniti, non nemo rem accusamus neque, quibusdam ab.
+            Libero, reprehenderit recusandae labore eaque, aspernatur aliquid
+            modi soluta ullam voluptatum unde repellat in molestias quod?
+            Doloribus ratione illum quisquam earum consequuntur nam voluptatem,
+            velit culpa maiores. Inventore esse illum excepturi dolores est
+            natus aliquid maiores sunt omnis deserunt accusantium amet enim eos
+            adipisci minima hic, nam provident quo aperiam quasi vel?
+            Dignissimos accusantium aliquid aut vel explicabo voluptatum
+            molestias fugit harum reprehenderit incidunt. Quis cumque explicabo
+            omnis officiis autem dolorum!
           </Text>
         </Stack>
       </Container>
@@ -71,27 +79,29 @@ const MyCarousel = () => (
     showArrows={false}
   >
     <Box w="full" h={'100vh'}>
-      <Image src={img1} />
+      <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
       <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
-        Watch the future
+        Watch The Future
       </Heading>
     </Box>
     <Box w="full" h={'100vh'}>
-      <Image src={img2} />
-      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+      <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
         Future is Gaming
       </Heading>
     </Box>
+
     <Box w="full" h={'100vh'}>
-      <Image src={img3} />
+      <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
-        Gaming on console
+        Gaming on Console
       </Heading>
     </Box>
+
     <Box w="full" h={'100vh'}>
-      <Image src={img4} />
+      <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
-        Night life
+        Night life is cool
       </Heading>
     </Box>
   </Carousel>
